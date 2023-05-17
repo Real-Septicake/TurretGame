fc908167f8051df4c3dadec3af8a66de13807079
septicake <septicake056@gmail.com>
2023-05-17 09:16:10 -0400

    Started work on Upgrade Paths for turrets

--- src/Upgrade.java
+++ src/Upgrade.java
@@ -1,19 +1,22 @@
 import greenfoot.*;
 
 public class Upgrade extends Actor {
+    private final static GreenfootImage PLACEHOLDER = new GreenfootImage("UpgradePlaceholderImage.png");
     private final double RANGE;
     private final int DAMAGE;
     private final int COOLDOWN;
     private final GreenfootImage IMAGE;
     private final String NAME;
     private final int ANIMATION_LENGTH_CHANGE;
-    public Upgrade(double r, int d, int cd, int a, GreenfootImage image, String n){
+    private final int COST;
+    public Upgrade(double r, int d, int cd, int a, GreenfootImage image, String n, int c){
         RANGE = r;
         DAMAGE = d;
         COOLDOWN = cd;
         ANIMATION_LENGTH_CHANGE = a;
         NAME = n;
-        IMAGE = image;
+        IMAGE = (image == null)?PLACEHOLDER:image;
+        COST = c;
     }
     public double getRange(){
         return RANGE;
@@ -31,6 +34,10 @@ public class Upgrade extends Actor {
     }
     public GreenfootImage getImage() {
         return IMAGE;
+    }
+
+    public int getCost() {
+        return COST;
     }
 
     public String getName() {
---
+++ src/UpgradePath.java
@@ -0,0 +1,28 @@
+import greenfoot.*;
+
+public class UpgradePath extends Actor {
+    private int upgradeNum = 0;
+    private final Upgrade[] upgrades;
+    private final TurretGunBase TURRET;
+    public UpgradePath(TurretGunBase t, Upgrade... u){
+        upgrades = u;
+        updateImage();
+        TURRET = t;
+    }
+    public void act(){
+        if(Greenfoot.mouseClicked(this) && Cash.getCash() >= upgrades[upgradeNum].getCost()){
+            Cash.alterCash(upgrades[upgradeNum].getCost());
+            TURRET.applyUpgrade(upgrades[upgradeNum++]);
+            updateImage();
+        }
+    }
+    public void updateImage(){
+        GreenfootImage gi = new GreenfootImage(120, 168);
+        gi.drawImage(upgrades[upgradeNum].getImage(), 0, 0);
+        GreenfootImage text = new GreenfootImage(upgrades[upgradeNum].getName(), 24, Color.BLACK, null);
+        gi.drawImage(text, gi.getWidth()/2 - text.getWidth()/2, 120);
+        GreenfootImage text2 = new GreenfootImage("$"+upgrades[upgradeNum].getCost(), 24, Color.BLACK, null);
+        gi.drawImage(text2, (gi.getWidth()/2) - text2.getWidth()/2, 144);
+        setImage(gi);
+    }
+}
